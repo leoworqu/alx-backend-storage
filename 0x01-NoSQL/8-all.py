449d02c14b954368c616d896615e8d58eb81c793
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-14 19:32:05 +0300

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python function that lists all documents in a collection
+"""
 
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find({}))
