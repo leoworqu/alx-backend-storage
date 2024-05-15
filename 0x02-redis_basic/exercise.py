f187c6b6c3952fd8c7bf503f230c771f794858d0
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-15 22:12:50 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -25,6 +25,26 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """
+    Wrapper for decorator functionality
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper for decorator functionality
+        """
+        inputs_key = method.__qualname__ + ":inputs"
+        outputs_key = method.__qualname__ + ":outputs"
+        
+        self._redis.rpush(inputs_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, str(output))
+        
+        return output
+    return wrapper
+
+
 class Cache:
     """
     Redis Cache Class
