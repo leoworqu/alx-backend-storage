23776f790c9bd2977cfa071b1310dc7ac550f5ab
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-15 22:01:04 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,6 +7,21 @@ import redis
 from typing import Union
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    count calls function decorate
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        Wrapper for decorator functionality
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
 class Cache:
     """
     Redis Cache Class
