99d142b8aab2795abd8d710347629f993a869f7f
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-15 21:53:51 +0300

    Create Exercise2.py

---
+++ 0x02-redis_basic/Exercise2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Practice with redis
+"""
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    """
+    Redis Cache Class
+    """
+    def __init__(self):
+        """
+        Constructor
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in redis
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """
+        Get data from redis
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        get str method
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        get int method
+        """
+        return self.get(key, fn=int)
