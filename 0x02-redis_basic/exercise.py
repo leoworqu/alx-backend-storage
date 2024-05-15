53ffb93b420acb505f9165066e42d5a57d1cc51d
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-15 22:15:45 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -45,6 +45,32 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(fn: Callable):
+    """
+    Display the history of calls of a particular function
+    """
+    r = redis.Redis()
+    f_name = fn.__qualname__
+    n_calls = r.get(f_name)
+    try:
+        n_calls = n_calls.decode('utf-8')
+    except Exception:
+        n_calls = 0
+    print(f'{f_name} was called {n_calls} times:')
+    inside = r.lrange(f_name + ":inputs", 0, -1)
+    outside = r.lrange(f_name + ":outputs", 0, -1)
+    for key, value in zip(inside, outside):
+        try:
+            key = key.decode('utf-8')
+        except Exception:
+            key = ""
+        try:
+            value = value.decode('utf-8')
+        except Exception:
+            value = ""
+        print(f'{f_name}(*{key}) -> {value}')
+
+
 class Cache:
     """
     Redis Cache Class
