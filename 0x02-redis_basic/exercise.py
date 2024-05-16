96d25916a4c81a6e49dfdc3daa1e3d1fa8302376
leoworqu <132668626+leoworqu@users.noreply.github.com>
2024-05-16 06:08:08 +0300

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -68,35 +68,7 @@ def replay(fn: Callable):
             value = value.decode('utf-8')
         except Exception:
             value = ""
-        print(f'{f_name}(*{key}) -> {value}')
-
-
-class Cache:
-    """
-    Redis Cache Class
-    """
-    def __init__(self):
-        """
-        Constructor
-        """
-        self._redis = redis.Redis()
-        self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Store data in redis
-        """
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
-
-    #!/usr/bin/env python3
-"""
-Practice with redis
-"""
-import uuid
-import redis
-from typing import Callable, Union
+        print(f'{f_name}(*{key}) -> {value}')  
 
 
 class Cache:
